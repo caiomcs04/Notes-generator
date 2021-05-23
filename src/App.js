f7c13d69d7ecf59397fcf4d5bae507c66041518a
caiomcs04 <caio6c@yahoo.com.br>
2021-05-22 23:36:10 -0300

    2#COMMIT(CSS)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import NotesList from "./components/NotesList/NotesList"
 import RegisterForm from "./components/RegisterForm/RegisterForm"
+import "./assets/App.css"
+import "./assets/index.css"
 
 
 function App() {
   return (
-    <>
+    <section className="conteudo">
       <RegisterForm />
       <NotesList />
-    </>
+    </section>
   )
 
 }
--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -2,20 +2,31 @@ import React, { Component } from "react";
 import "./style.css"
 
 class RegisterForm extends Component {
+
+  constructor() {
+    super()
+    this.title = ""
+  }
+
+  handleTitle(event) {
+    this.title = event.target.value
+  }
+
   render() {
     return (
       <>
-      <form>
-        <input type="text" placeholder="Título">
-
-        </input>
-        <textarea placeholder="Escreva sua nota">
-
-        </textarea>
-        <button>
-          Criar nota
+      {console.log()}
+        <form className="register-form"
+        >
+          <input type="text"
+            placeholder="Título"
+            className="register-form-input"
+            onChange={this.handleTitle} />
+          <textarea placeholder="Escreva sua nota" className="register-form-input" />
+          <button className="register-form-input register-form-submit">
+            Criar nota
         </button>
-      </form>
+        </form>
       </>
     )
   }
